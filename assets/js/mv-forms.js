8a7c15fbf5a9574016b34dfa7d9c29f2e04e8465
Ryan Martell <ryan@martellventures.com>
2017-09-27 14:05:41 -0400

    leave backward compatibility in ng-mv.

--- assets/js/mv-forms.js
+++ assets/js/mv-forms.js
@@ -11,11 +11,23 @@
 		var countryLoadDeferred = $q.defer();
 	
 		var getResult = $http.get($mvConfiguration.templateBasePath+'countries.json');
-          getResult.then(function(responseData, status, headers, config) { 
-               countryLoadDeferred.resolve(responseData.data);
-          }, function(data, status, headers, config, statusText) {
-               countryLoadDeferred.reject(status);
-          });
+		
+		if(getResult.success !== undefined)
+		{
+			getResult.success(function(data) {
+				countryLoadDeferred.resolve(data);
+			});
+			getResult.error(function(failureData) {
+				countryLoadDeferred.reject(failureData);
+			});
+		} else {
+			// modern versions of angular unify this with then.
+			getResult.then(function(responseData) {
+				countryLoadDeferred.resolve(responseData.data);
+			}, function(data, status) {
+				countryLoadDeferred.reject(status);
+			});
+		}
 
 		return countryLoadDeferred.promise;
 	}]);
